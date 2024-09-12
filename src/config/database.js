55352bf74a9ad08611ed6f011cb2b0111ea67a1e
AbderahmaneThimbo <abderahmanethimbo@gmail.com>
2024-09-12 12:37:02 +0000

    Exceptions lors de la connexion à la base

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+
 const db = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
@@ -6,9 +7,16 @@ const db = mysql.createConnection({
   database: 'order_manager'
 });
 
-db.connect(function (err) {
-  if (err) throw err;
-  console.log("Connecté à la base de données MySQL!");
-});
+try {
+  db.connect(function (err) {
+    if (err) {
+      console.error("Erreur de connexion à la base de données :", err.message);
+      return;
+    }
+    console.log("Connecté à la base de données MySQL !");
+  });
+} catch (err) {
+  console.error("Erreur inattendue lors de la tentative de connexion :", err.message);
+}
 
 module.exports = db;
